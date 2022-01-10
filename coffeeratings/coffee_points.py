f0bc404d9d0f7acf3e85a837429dea3b7d49a5c1
isabelizimm <isabel.zimmerman@rstudio.com>
2022-01-10 15:36:34 -0500

    update coffee ratings to use vetiver server

--- coffeeratings/coffee_points.py
+++ coffeeratings/coffee_points.py
@@ -2,12 +2,22 @@ import pandas as pd
 from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 import numpy as np
-from joblib import dump
+from pydantic import BaseModel
+import vetiver
 
 np.random.seed(500)
 
 
 raw = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv')
+
+class Coffee(BaseModel):
+    aroma: float
+    flavor: float
+    sweetness: float
+    acidity: float
+    body: float
+    uniformity: float
+    balance: int
 
 df = pd.DataFrame(raw)
 coffee = df[["total_cup_points", "aroma", "flavor", "sweetness", "acidity", \
@@ -17,4 +27,4 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(coffee.iloc[
 
 lr = LinearRegression().fit(X_train, y_train)
 
-dump(lr, 'coffeelr.joblib')
+vetiver.vetiver_serve(lr, Coffee)
--- coffeeratings/service.py
+++ coffeeratings/service.py
@@ -1,53 +1,75 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 import joblib
+from joblib import dump
 import uvicorn
 import sklearn
+import pandas as pd
+from typing import List
 
-model = joblib.load("coffeelr.joblib")
-app = FastAPI()
-
-class Coffee(BaseModel):
-    aroma: float
-    flavor: float
-    sweetness: float
-    acidity: float
-    body: float
-    uniformity: float
-    balance: float
+# do we want to convert to joblib?
+def _prepare_model(model):
+    dump(model, 'vetiver_model.joblib')
+    load_model = joblib.load("vetiver_model.joblib")
+    return(load_model)
 
 
-@app.get("/")
-def vetiver_intro():
-    return {"message": "go to /rapidoc or /docs"}
+def _validate_data(data_base_model, pred_data):
+    
+    class Validate(BaseModel):
+        df_dict: List[data_base_model]
 
-@app.post("/predict")
-async def prediction(pred_data: Coffee):
+    try:
+        Validate(df_dict = pred_data.dict())
+    except ValidationError as e:
+        print(e.json())
 
-    data = pred_data.dict()
-    data_in = [[data['aroma'], data['flavor'], data['sweetness'], data['acidity'], data['body'], data['uniformity'], data['balance']]]
+    dict_data = pred_data
+    served_data = []
 
-    y = model.predict(data_in)
+    for key, value in dict_data:
+        served_data.append(value)
 
-    return {'prediction': y[0]}
+    return served_data
 
-@app.get("/rapidoc", response_class=HTMLResponse, include_in_schema=False)
-async def rapidoc():
-    return f"""
-        <!doctype html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <script 
-                    type="module" 
-                    src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"
-                ></script>
-            </head>
-            <body>
-                <rapi-doc spec-url="{app.openapi_url}"></rapi-doc>
-            </body> 
-        </html>
-    """
 
-uvicorn.run(app, host="127.0.0.1", port=8000)
+def vetiver_serve(sk_model, data_base_model, host_addr = "127.0.0.1", port = 8000):
+    
+    served_model = _prepare_model(sk_model)
+
+    # create a template for visual docs?
+
+    app = FastAPI()
+    @app.get("/")
+    def vetiver_intro():
+        return {"message": "go to /rapidoc or /docs"}
+
+    @app.post("/predict")
+    async def prediction(pred_data: data_base_model):
+              
+        served_data = _validate_data(data_base_model, pred_data)
+
+        y = served_model.predict([served_data])
+
+        return {'prediction': y[0]}
+
+    @app.get("/rapidoc", response_class=HTMLResponse, include_in_schema=False)
+    async def rapidoc():
+        return f"""
+            <!doctype html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <script 
+                        type="module" 
+                        src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"
+                    ></script>
+                </head>
+                <body>
+                    <rapi-doc spec-url="{app.openapi_url}"></rapi-doc>
+                </body> 
+            </html>
+        """
+
+    uvicorn.run(app, host=host_addr, port=port)
